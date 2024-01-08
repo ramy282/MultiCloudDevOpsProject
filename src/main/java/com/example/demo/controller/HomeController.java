6100760d738efe29d9b199cc69f610d87397b10c
Mohamed ElEmam <Emam@MacBook-Pro.local>
2024-01-08 18:54:31 +0200

    Add My App

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "index"; // This assumes you have an "index.html" in your resources/templates folder
+    public String home(Model model) {
+        try {
+            // Get the local IP address of the server
+            String ipAddress = InetAddress.getLocalHost().getHostAddress();
+            model.addAttribute("ipAddress", ipAddress);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            model.addAttribute("ipAddress", "Unable to fetch IP");
+        }
+
+        return "index";
     }
 }
