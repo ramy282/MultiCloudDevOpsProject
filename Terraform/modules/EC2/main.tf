8669a27d7a9dc9ce2c747ae3fc7c3c8212e82fd2
ramy282 <60857262+ramy282@users.noreply.github.com>
2024-06-12 21:07:26 +0300

    Add files via upload

---
+++ Terraform/modules/EC2/main.tf
@@ -0,0 +1,33 @@
+data "aws_ami" "amazon_ec2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+   filter {
+     name    = "virtualization-type"
+     values  = ["hvm"]
+   }
+
+   owners = ["099720109477"] 
+}
+
+
+resource "aws_instance" "app" {
+  count				= length(var.sub_pub)
+  ami				= data.aws_ami.amazon_ec2.image_id 
+  instance_type			= var.type
+  subnet_id			= var.sub_pub[count.index]
+  vpc_security_group_ids	= [var.app_sg_id]
+  monitoring 			= true 
+  associate_public_ip_address 	= true
+  key_name                    	= var.key-pair
+
+  tags = {
+    Name = "instance-${count.index}"
+  }
+}
+
+
