13bcbd13e7b114595eaf41636bfc8cf2a456418c
ramy282 <60857262+ramy282@users.noreply.github.com>
2024-06-12 21:14:46 +0300

    Add files via upload

---
+++ Terraform/modules/routetable/outputs.tf
@@ -0,0 +1,3 @@
+output "route_table" {
+  value = aws_route_table.public_route_table.id
+}
---
+++ Terraform/modules/routetable/variables.tf
@@ -0,0 +1,23 @@
+variable "route-cidr" {
+  type		= string
+}
+
+variable "tag" {
+  type          = string
+
+}
+
+variable "iVolve" {
+  type          = string
+
+
+}
+
+variable "igw" {
+  type          = string
+
+}
+
+variable "sub_pub" {
+  
+}
