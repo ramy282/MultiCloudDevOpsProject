1c7549be08210e161bdc6e6537cd046a576da98a
ramy282 <60857262+ramy282@users.noreply.github.com>
2024-06-18 22:25:44 +0300

    Update sonarQubeAnalysis.groovy

--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -1,8 +1,25 @@
 #!/usr/bin/env groovy
 
-def call(){ 
-	withSonarQubeEnv() { 
-		    sh 'chmod +x gradlew'
-        	sh "./gradlew sonarqube-server" 
-	}
+def call() {
+    echo "Running SonarQube Analysis..."
+
+    // Define your SonarQube server configuration credentials ID
+    def sonarqubeCredentialsId = 'sonar-token'
+
+    // Define other properties for SonarQube analysis
+    def projectName = 'ivolve-Grad-project'
+    def projectKey = 'ivolve-Grad-project'
+
+    // Run withSonarQubeEnv to inject SonarQube environment variables
+    withSonarQubeEnv(credentialsId: sonarqubeCredentialsId) {
+        // Assuming you have a SonarQube tool installation named 'SonarQubeScanner'
+        def scannerHome = tool name: 'sonar-scanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+        
+        sh """
+        ${scannerHome}/bin/sonar-scanner \
+          -Dsonar.projectName=${projectName} \
+          -Dsonar.projectKey=${projectKey} \
+          -Dsonar.java.binaries=.
+        """
+    }
 }
