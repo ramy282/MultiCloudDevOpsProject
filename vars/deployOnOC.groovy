8088cba88146194a557b1a4ba63850b83b81bbf0
ramy282 <60857262+ramy282@users.noreply.github.com>
2024-06-19 17:03:38 +0300

    Update deployOnOC.groovy

--- vars/deployOnOC.groovy
+++ vars/deployOnOC.groovy
@@ -4,7 +4,7 @@ def call(String OpenShiftCredentialsID, String openshiftClusterurl, String opens
     // login to OpenShift Cluster via cluster url & service account token
     withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) {
             sh "oc login --server=${openshiftClusterurl} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify"
-            sh "oc apply -f deployment.yml"
+            sh "oc apply -f Deployment.yml"
             sh "oc apply -f service.yml"
             sh "oc apply -f route.yml"
     }
